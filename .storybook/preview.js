8cb08f048d0c5c1f301e2a3b4b0023dbb8625ac3
himchan94 <himchan9494@gmail.com>
2022-05-22 18:44:05 +0900

    feat: Apply msw to project

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-
 import * as NextImage from "next/image";
+import { setupWorker, rest } from "msw";
 
 const OriginalNextImage = NextImage.default;
 
@@ -8,6 +8,15 @@ Object.defineProperty(NextImage, "default", {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
+
+if (typeof global.process === "undefined") {
+  const worker = setupWorker(
+    rest.get("https://localhost:3000/api/hello", (req, res, ctx) => {
+      return res(ctx.json({ name: "John Doh" }));
+    })
+  );
+  worker.start();
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
